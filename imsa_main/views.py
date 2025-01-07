de8710f68b9a6c23b108a5c1fd36392c41482b9f
Nekitmm5cs <Nekitmm5cs@users.noreply.github.com>
2025-01-07 15:22:00 +0300

    ready reg1

--- imsa_main/views.py
+++ imsa_main/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
+# Главная страница
 def main(request):
-    return render(request, "imsa_main/main.html")
+    if request.user.is_authenticated:
+        return render(request, 'imsa_main/main.html', {'user_authenticated': True})
+    return render(request, 'imsa_main/main.html', {'user_authenticated': False})
--- imsa_users/views.py
+++ imsa_users/views.py
@@ -1,7 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as django_login, logout
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
-def login(request):
-    return render(request, "imsa_users/login.html")
+# Представление для логина
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            django_login(request, user)  # Логиним пользователя
+            return redirect('main')  # Перенаправление на главную страницу
+        else:
+            messages.error(request, 'Неверный логин или пароль')
+    return render(request, 'imsa_users/login.html')
 
+# Представление для регистрации
 def register(request):
-    return render(request, "imsa_users/register.html")
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Пользователь с таким именем уже существует')
+        else:
+            user = User.objects.create_user(username=username, email=email, password=password)
+            user.save()
+            messages.success(request, 'Аккаунт успешно создан')
+            return redirect('login')  # Перенаправление на страницу логина
+    return render(request, 'imsa_users/register.html')
+
+# Выход из аккаунта
+def logout_view(request):
+    logout(request)
+    return redirect('main')  # Перенаправление на главную страницу после выхода
+
+@login_required
+def profile(request):
+    return render(request, 'imsa_users/profile.html')
